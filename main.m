f1e62346a5a864e38a85ca7f203b5594ba61fbd2
Nikhil Sampangi <33094095+nikhilsampangi@users.noreply.github.com>
2018-10-16 03:05:07 +0530

    Add files via upload
    structure

--- main.m
+++ main.m
@@ -1,10 +1,26 @@
 % Main File
-clear all; 
 close all; 
 clc;
 
+%Taking input from the data-set
 input = 'dataset/sample.wav';
-g = 50;
-transitionV = [1000, 1500, 2550, 5000];
-Psat=100;
-y = hearingAidF(input,g,Psat,transitionV,newfile);
+
+%gain that has to be applied to the signal
+g = ;
+
+%
+transitionV = ;
+
+%Saturation Power
+Psat= ;
+%reading the input file
+[ip,sf] = audioread(input);
+
+%Applying a low-pass filter with frequency of 20,000Hz which is maximum
+%frequency audible to normal human ears.
+f_pass = 20000;
+ip_f = lowpass(ip,f_pass,sf);
+
+% Addition of gaussian noise to the input signal
+
+
