a387b6300f141a41634faea4bad9380b9df10b1f
Nikhil Sampangi <nikhil.sampangi@gmail.com>
2018-12-12 12:24:34 +0530

    V_2

--- Denoise.m
+++ Denoise.m
@@ -1,11 +1,27 @@
-%Denoising filter using Wavelet Toolbox
+%   Denoising filter using Wavelet Toolbox
+%   A wavelet is a mathematical function useful in digital signal processing and image compression
 
 function y = Denoise(x)
 
-%THR is the threshold, SORH is for soft or hard thresholding, KEEPAPP allows you to keep 
-%approximation coefficients
-
+%   ddencmp returns default values for denoising or compression for the critically-sampled 
+%   discrete wavelet or wavelet packet transform.
+%   Example: [THR,SORH,KEEPAPP,CRIT] = ddencmp(IN1,IN2,X)
+%   THR is the threshold, SORH is for soft or hard thresholding, KEEPAPP allows you to keep 
+%   approximation coefficients
+%   IN1 is 'den' for denoising
+%   IN2 is 'wv' for wavelet
+%   X : input signal
 [thr,sorh,keepapp]=ddencmp( 'den' , 'wv' ,x);  
 
-% returns a de-noised version xc of input signal x (our one-dimensional speech signal)
-[y, ~, ~, ~, ~]=wdencmp( 'gbl' ,x, 'db3' ,2,thr,sorh,keepapp);  
+% wdencmp does De-noising of the audio signal
+% returns a de-noised version y of input signal x (our one-dimensional speech signal)
+% [XC,CXC,LXC,PERF0,PERFL2] = wdencmp('gbl',X,'wname',N,THR,SORH,KEEPAPP)
+% 'wname' is a character vector containing wavelet name, db stands for
+% 'Daubechies wavelets'
+
+[y, ~, ~, ~, ~]=wdencmp( 'gbl' ,x, 'db3' ,2,thr,sorh,keepapp);
+
+figure;
+plot(y),title('Input Signal after de-noising')
+
+
--- main.m
+++ main.m
@@ -1,24 +1,24 @@
+function main(ip, fsr, g_max, p_sat, tr_1, tr_2, tr_3, tr_4)
+
 % Main File
 
-%Inputs
-ip_path =input('Input file: ');
-g_max = input('Max gain: ');
-p_sat = input('Saturation power: ');
-tr_v = input('Frequency Transition Vector: ');
+%ip  : input
+%fsr : frequency sampling rate
+%g_max : maximum gain to be applied
+%p_sat : Saturation power
+%tr_ : Frequency transition values
 
-%Reading Audio file
-[ip,fsr] = audioread(ip_path);
-ip = ip(:, 1);
+%ip = ip(:,1); % Taking one channel of the audio
+
 figure;
 plot(ip),title('Input Signal')
 
 %Applying a low-pass filter with frequency of 20,000Hz which is maximum
 %frequency audible to normal human ears.
+
 f_pass = 20000;
 ip2 = lowpass(ip,f_pass,fsr);
 
-%figure;
-%plot(ip2),title('Input Signal after low pass')
 
 % Addition of white gaussian noise to the input signal
 %
@@ -28,21 +28,19 @@ ip2 = lowpass(ip,f_pass,fsr);
 %White Noise: A random process signal with a constant 
 %power spectral density (PSD) function is a white noise process.
 
+% awgn(in,snr) : snr : signal to noise ratio
 ip3 = awgn(ip2,30);
 figure;
 plot(ip3),title('Input Signal after addition of Noise')
 
-% denoising filter
 
-ip_de_n = Denoise(ip3);  
-figure;
-plot(ip_de_n),title('Input Signal after de-noising')
+% denoising filter
+ip_de_n = Denoise(ip3); 
+
 
 % frequency shaping filter
 
-ip_f = FreqShape(ip_de_n,g_max,tr_v,fsr);
-figure;
-plot(ip_f),title('Input Signal after frequency shaping')
+ip_f = FreqShape(ip_de_n,g_max,tr_1,tr_2,tr_3,tr_4,fsr);
 
 % amplitude compression filter
 
@@ -80,6 +78,6 @@ specgram(ip_a);
 title('Spectrogram of Adjusted Signal 2');
 
 
-%soundsc(input, fs);
-sound(ip_a,fsr);
-%auwrite(y,fs,info.BitsPerSample,'linear',newfile);
+
+soundsc(ip_a,fsr);
+%auwrite(ip_a,fsr,info.BitsPerSample,'linear',newfile);
